7296b55c8fca531a53060b2ae0de24032782a94f
xdz987 <1415187987@qq.com>
2021-03-12 00:12:04 +0800

    add q992、q1234、q1658

---
+++ src/数组与矩阵/滑动窗口/q1234_2_替换子串得到平衡字符串/Solution.java
@@ -0,0 +1,140 @@
+package 数组与矩阵.滑动窗口.q1234_2_替换子串得到平衡字符串;
+
+import java.util.HashMap;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "QQWERWWW";
+        BalancedString balancedString = new BalancedString();
+        System.out.println(balancedString.balancedString(s));
+    }
+}
+
+/**
+ * 解题：关注多余平均数的字符，如Q:1 W:4 E:3 R:4 length/4=3，关注W和R，满足条件最小替换即"WR"替换成"QQ"，长度为2。又或者WQR，长度为3
+ * 满足条件最小窗口 HashMap(效率低 不推荐)
+ * 满足条件最小窗口 int[]，范围单字符可转换ASCII实现mapInt[]形式
+ */
+class BalancedString {
+    public int balancedString(String s) {
+        //(1)初始化窗口指针、结果res、s各个字符数量容器
+        int length = s.length();
+        int left = 0;
+        int right = 0;
+        int balanceLen = length / 4;
+        int res = length;
+        int[] count = new int[26];
+
+        //(2)统计字符数量
+        for (int i = 0; i < length; i++) {
+            count[s.charAt(i) - 'A']++;
+        }
+
+        //滑动窗口：以减的形式 PS：此可节约空间，即节省用于验证的容器
+        while (right < length) {
+            //(3)right向右拓展窗口，减少字符数量直到满足条件(没有字符数量大于balanceLen)
+            int rChar = s.charAt(right);
+            count[rChar - 'A']--;
+            right++;
+
+            //(4)left向右缩小窗口
+            while (left < length &&
+                    count['Q' - 'A'] <= balanceLen &&
+                    count['E' - 'A'] <= balanceLen &&
+                    count['W' - 'A'] <= balanceLen &&
+                    count['R' - 'A'] <= balanceLen
+            ) {
+                //(5)保存最小res
+                res = Math.min(res, right - left);
+                int lChar = s.charAt(left);
+                count[lChar - 'A']++;
+                left++;
+            }
+        }
+
+        return res;
+    }
+}
+class BalancedStringMap {
+    public int balancedString(String s) {
+        //(1)初始化窗口指针、结果res、窗口元素容器window、目标元素容器need
+        int left = 0;
+        int right = 0;
+        int res = Integer.MAX_VALUE;
+        HashMap<Character, Integer> window = new HashMap<>();
+        HashMap<Character, Integer> need = needReplaceChar(s);
+
+        //说明已经是平衡字符串
+        if (need.isEmpty()) {
+            return 0;
+        }
+
+        System.out.println(need);
+        //滑动窗口
+        while (right < s.length()) {
+            char rChar = s.charAt(right);
+            right++;
+            window.put(rChar, window.getOrDefault(rChar, 0) + 1);
+
+            //当满足替换要求时缩小窗口
+            while (isSatisfied(need, window)) {
+                res = Math.min(res, right - left);
+
+                char lChar = s.charAt(left);
+                left++;
+                window.put(lChar, window.get(lChar) - 1);
+            }
+        }
+
+        return res;
+    }
+
+    //只有满足即>=need中字符中数量即可
+    private boolean isSatisfied(HashMap<Character, Integer> need, HashMap<Character, Integer> window) {
+        boolean res = true;
+        for (char c : need.keySet()) {
+            //window不含need的元素直接false
+            if (need.containsKey(c) && !window.containsKey(c)) {
+                res = false;
+                break;
+            } else if (window.get(c) < need.get(c)) {
+                res = false;
+                break;
+            }
+        }
+
+        return res;
+    }
+
+    //返回"Q","W","E","R"中缺少char的map
+    private HashMap<Character, Integer> needReplaceChar(String s) {
+        String balance = "QWER";
+        int length = s.length();
+        int balanceLen = length / 4;
+        HashMap<Character, Integer> need = new HashMap<>();
+
+        //初始化need容器
+        for (char c : balance.toCharArray()) {
+            need.put(c, 0);
+        }
+
+        //统计字符
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            need.put(c, need.get(c) + 1);
+        }
+
+        //更新need为需要查找字符的map
+        for (int i = 0; i < 4; i++) {
+            char c = balance.charAt(i);
+            int cLen = need.get(c);
+            if (cLen <= balanceLen) {
+                need.remove(c);
+            } else {
+                need.put(c, need.get(c) - balanceLen);
+            }
+        }
+
+        return need;
+    }
+}
---
+++ src/数组与矩阵/滑动窗口/q992_3_K个不同整数的子数组/Solution.java
@@ -0,0 +1,73 @@
+package 数组与矩阵.滑动窗口.q992_3_K个不同整数的子数组;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] A = new int[]{1, 2, 1, 2, 3};
+        int K = 2;
+        SubarraysWithKDistinct subarraysWithKDistinct = new SubarraysWithKDistinct();
+        System.out.println(subarraysWithKDistinct.subarraysWithKDistinct(A, K));
+    }
+}
+
+/**
+ * 解题：
+ * 由于题目要求的是，某个子数组中不同的整数个数恰好(等于)为K，直接使用滑动窗口比较难求(如例1)。
+ * 可运用技巧的是：恰好(等于)K可转换为最多为K，即：
+ * (1)例如K=2。  最多为2则是：可以是1个整数或2个整数； 恰好为2：只能是2个整数
+ * (2)即最多2 = 恰好为2 + 恰好为1/最多为1
+ * (3)即恰好K = 最多K - 最多K-1
+ * 而转换为最多为K后，则滑动窗口的计算变得很紧密。本来恰好为K需要跳过K-1...的情况，滑动窗口变得麻烦如(K=2,跳过1)；而最多为K则可以计算所有的可能，无需考虑K-1及往前的计算(如例2)。
+ * <p>
+ * 例1:[1,2,1,2,3]，k=2。滑动窗口[1,2][1,2,1][1,2,1,2][2,3]只有4种，而少了3种[2,1][2,1,2][1,2]
+ * 例2:[1,2,1,2,3]，K最多为2==>K=1或2。
+ * K最多为2时：[1][1,2][1,2,1][1,2,1,2] [2][2,1][2,1,2] [1][1,2] [2][2,3] [3] = 12
+ * K最多为1时，[1][2][1][2][3] = 5
+ * 恰好2 = 最多为2 - 最多为1 = 12 - 5 = 7种
+ * <p>
+ * 最多K的解法：
+ * 滑动窗口上面例2：
+ * 当窗口指针left或right固定(即在收缩或者拓展窗口处)时：res+=right-left(如right指向窗口最后一个元素，则+1)
+ * 如left固定而向右拓展窗口：[1,2,1,2,3],k=2。 left=0固定时，直到K>2之前为 [1][1,2][1,2,1][1,2,1,2] 再收缩则[2][2,1][2,1,2] ... 直到[2][2,3] ==> 即4+3+2+1+2=12
+ */
+class SubarraysWithKDistinct {
+
+    public int subarraysWithKDistinct(int[] A, int K) {
+        return atMostDistinct(A, K) - atMostDistinct(A, K - 1);
+    }
+
+    private int atMostDistinct(int[] A, int K) {
+        //(1)初始化窗口指针、不同整数数量valid、不同整数统计freq、结果res
+        int length = A.length;
+        int left = 0;
+        int right = 0;
+        int valid = 0;
+        int[] freq = new int[length + 1];
+        int res = 0;
+
+        //滑动窗口
+        while (right < length) {
+            //(2)right向右拓展窗口，更新freq和valid
+            int rNum = A[right];
+            if (freq[rNum] == 0) {
+                valid++;
+            }
+            freq[rNum]++;
+            right++;
+
+            //(3)不满足条件时left向右缩小窗口，更新freq和valid
+            while (valid > K) {
+                int lNum = A[left];
+                freq[lNum]--;
+                if (freq[lNum] == 0) {
+                    valid--;
+                }
+                left++;
+            }
+
+            //(4)保存当前窗口元素的right-left种子数组数量
+            res += right - left;
+        }
+
+        return res;
+    }
+}
